8d966485d0c1eaf622685a4bf01214461cb18098
Tim Child <timjchild@gmail.com>
2025-03-28 14:59:50 -0700

    add a demo upload handler

--- receipt_reader/receipt_reader.py
+++ receipt_reader/receipt_reader.py
@@ -1,6 +1,10 @@
 """Welcome to Reflex! This file outlines the steps to create a basic app."""
 
+import asyncio
+from typing import AsyncIterator
+
 import reflex as rx
+from reflex.event import EventType
 
 from rxconfig import config
 
@@ -8,7 +12,17 @@ from rxconfig import config
 class State(rx.State):
     """The app state."""
 
-    ...
+    @rx.event(background=True)
+    async def on_drop_process_receipt(self, files: rx.UploadFile) -> AsyncIterator[EventType]:
+        """Process the receipt."""
+        _ = files
+        yield rx.toast.info("Processing receipt...")
+
+        await asyncio.sleep(2)
+        # Do the processing here
+
+        yield rx.toast.info("Receipt processed!")
+        return
 
 
 def index() -> rx.Component:
@@ -16,6 +30,8 @@ def index() -> rx.Component:
     return rx.container(
         rx.color_mode.button(position="top-right"),
         rx.vstack(
+            rx.button("Demo upload", on_click=lambda: State.on_drop_process_receipt(None)),  # pyright: ignore[reportArgumentType]
+            rx.upload(on_drop=State.on_drop_process_receipt, multiple=False),
             rx.heading("Welcome to Reflex!", size="9"),
             rx.text(
                 "Get started by editing ",
