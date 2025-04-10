c6413e019f3824c1b8f346d3882613933641b4dc
Tim Child <timjchild@gmail.com>
2025-04-10 13:22:47 -0700

    add failing test for convert line item

--- tests/test_response.py
+++ tests/test_response.py
@@ -1,4 +1,5 @@
 import pytest
+from pydantic import BaseModel
 
 from receipt_reader.response_models import LineItem, LineItemExpenseField, ValueDetection
 
@@ -12,6 +13,18 @@ def line_item() -> LineItem:
     )
 
 
+class ItemInfo(BaseModel):
+    text: str
+    amount: float
+
+
 def test_line_item(line_item: LineItem):
     assert isinstance(line_item, LineItem)
     assert line_item.line_item_expense_fields[0].page_number == 1
+    assert line_item.line_item_expense_fields[0].value_detection.text == "some text"
+
+
+def test_convert_to_item_infos(line_item: LineItem):
+    infos = convert_to_item_infos(line_item)
+    assert isinstance(infos, list)
+    assert all(isinstance(info, ItemInfo) for info in infos)
